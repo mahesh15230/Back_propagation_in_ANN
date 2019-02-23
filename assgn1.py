0d195f740409edabc46bfc3a4591eb1023b64a5d
Mahesh Yayi <ymc.yayi98@gmail.com>
2019-02-23 20:12:30 +0530

    added training ANN

--- assgn1.py
+++ assgn1.py
@@ -4,6 +4,7 @@ import copy as cp
 
 def data_init(datafile, test = None, train = None, valid = None, full = None):
 	data = cp.deepcopy(normalization(np.loadtxt(datafile)))
+	np.random.shuffle(data)
 	if full != None:
 		return data
 	testing_data = data[:round(len(data) * .1)]
@@ -63,7 +64,7 @@ def tanh(x, isbackward = False):
 	else:
 		return 1 - tanh(x,False)**2
 
-def forwardpass(normal_minibatch, no_act_layers, layers, weights, actfunc, minibatchsize, reg_coeff):
+def forwardpass(normal_minibatch, no_act_layers, layers, weights, actfunc, minibatchsize, reg_coeff, plot = False):
 	dkn = normal_minibatch[-1]
 	normal_minibatch = np.delete(normal_minibatch,np.shape(normal_minibatch)[0]-1,0)
 	layers[0][1:,:] = normal_minibatch[:,:minibatchsize]
@@ -75,25 +76,23 @@ def forwardpass(normal_minibatch, no_act_layers, layers, weights, actfunc, minib
 		else:
 			no_act_layers[i+1][:,:] = weights[i]@layers[i]
 			layers[i+1][:,:] = actfunc[i](weights[i]@layers[i],False)
-	error = np.subtract(dkn, actfunc[i+1](layers[-1], False))
+	error = np.subtract(dkn, actfunc[i+1](layers[i+1], False))
 	m = 0
 	for i in range(len(weights)):
 		m += sum(np.shape(weights[i]))
 	weight_reg = (reg_coeff / m) * sum([np.sum((weights[i]**2).reshape(np.shape(weights[i]))) for i in range(len(weights))])
 	error_energy = error**2 + weight_reg
-	return np.sum(error_energy) / (minibatchsize * 2)
+	if plot == False:
+		return error
+	else:
+		return np.sum(error_energy) / (minibatchsize * 2)
 
 def phidashv(layer): # Returns a row matrix
 	return np.array([layer.sum(axis=1)]) / np.shape(layer)[1]
 	
 def localgrad(localgradprev,weight,phidash): #localgradprev is row matrix, so is phidash
-	if np.shape(localgradprev)[1] == np.shape(weight)[0]:
-		if np.shape(weight)[1] == np.shape(phidash)[1]:
-			return phidash * (localgradprev @ weight)
-		else:
-			print('dim weight and phidash incompatible')
-	else:
-		print('dim localgrad and weight incompatible')
+	local_grad = phidash * (localgradprev @ weight)
+	return local_grad[:,1:]
 
 # Instructions to use the above function
 # localgradprev = np.array([[.3,.4,.5]])
@@ -103,28 +102,24 @@ def localgrad(localgradprev,weight,phidash): #localgradprev is row matrix, so is
 # print('test localgrad',localgrad(localgradprev,weight,phidash))
 
 
-def backprop(batch_avg_error, no_act_layers, actfunc, weights, layers, learning_param, minibatchsize, reg_coeff):
+def backprop(batch_avg_error, beta, no_act_layers, actfunc, weights, layers, learning_param, minibatchsize, reg_coeff):
 	phi_dash_vj = actfunc[-1](no_act_layers[-1], True)
 	avg_phi_dash = phi_dash_vj.sum(axis=1) / minibatchsize
 	local_grad = np.array([batch_avg_error * avg_phi_dash])  # Hardcoded to the assumption output layer has only one neuron
-	delta_weights = []
-	for i in range(len(layers) - 2, 0, -1):
-		print('!!!___i___!!!',i)
-		print('dim weight', np.shape(weights[i]))
-		print('dim localgrad', np.shape(np.transpose(local_grad)))
-		print('dim layer', np.shape(layers[i]))
-		print('dim phidashavg', np.shape(phidashv(layers[i])))
-		print('type weight update eq',np.shape(np.transpose(local_grad) @ phidashv(layers[i])))
-		delta_weights.append(learning_param * (np.transpose(local_grad) @ phidashv(layers[i])) - reg_coeff * abs(weights[i]))
-		#local_grad = sum(local_grad @ weights[i]) * layers[i-1].sum(axis = 1).reshape(1, np.shape(layers[i-1])[0]) / np.shape(layers[i-1])[1]
+	del_weight_prev = []
+	for i in range(len(layers) - 2, -1, -1):
+		print('__i__', i)
+		del_weight = learning_param * (np.transpose(local_grad) @ phidashv(layers[i])) - reg_coeff * abs(weights[i])
+		del_weight_prev.append(del_weight)
+		weights[i][:,:] += beta * del_weight
 		local_grad = localgrad(local_grad,weights[i],phidashv(layers[i]))
-
-
-
-
+	del_weight_prev = del_weight_prev[::-1]
+	for i in range(len(weights)):
+		weights[i][:,:] += (1 - beta) * del_weight_prev[i]
 
 datafile = 'dataset_minibatch_test.txt'
 fulldata = np.transpose(np.array(data_init(datafile, full = 1)))
+print('dim fulldata', np.shape(fulldata))
 ann_arch = [5,4,3,2,1]
 act = [tanh,relu,tanh,relu,logistic]
 minibatchsize = 10
@@ -134,4 +129,21 @@ layers = layers_init(ann_arch, minibatchsize)
 no_act_layers = layers_init(ann_arch, minibatchsize)
 weights = weights_init(ann_arch)
 error = forwardpass(fulldata[:minibatchsize,:], no_act_layers, layers, weights, act, minibatchsize, reg_coeff)
-print('back prop test', backprop(error, no_act_layers, act, weights, layers, learning_param, minibatchsize, reg_coeff))
+print('back prop test', backprop(error, .9, no_act_layers, act, weights, layers, learning_param, minibatchsize, reg_coeff))
+
+def trainingANN(data, epochs, minibatchsize, beta, learning_param, reg_coeff, ann_arch, no_act_layers, layers, actfunc, weights):
+	for j in range(epochs):
+		testdata = data_init(data, test = 1)
+		traindata = data_init(data, train = 1)
+		validdata = data_init(data, valid = 1)
+		trainingerror = []
+		validerror = []
+		for i in range(len(traindata)):
+			error = forwardpass(traindata[:,i:i + minibatchsize], no_act_layers, layers, weights, actfunc, minibatchsize, reg_coeff, plot = False)
+			trainingerror.append(training_error)
+			backprop(error, beta, no_act_layers, act, weights, layers, learning_param, minibatchsize, reg_coeff)
+		for i in range(len(validdata)):
+			validation_error = forwardpass(validdata[:,i:i + minibatchsize], no_act_layers, layers, weights, actfunc, minibatchsize, reg_coeff)
+			validerror.append(validation_error)
+
+
