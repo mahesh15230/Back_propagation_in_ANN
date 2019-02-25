4fc5783f7c4a0d9237c855c873c13cd859eff07d
mahesh15230 <36959431+mahesh15230@users.noreply.github.com>
2019-02-25 16:25:24 +0530

    Repo Update

--- assgn1.py
+++ assgn1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import copy as cp
+import matplotlib.pyplot as plt
 
 def normalization(data):
 	data_training = np.transpose(cp.deepcopy(data))	
@@ -72,22 +73,37 @@ class FP:
 		elif plot == False:
 			return np.sum(error) / len(error)
 
-def phidashv(layer): # Returns a row matrix
-	return np.array([layer.sum(axis=1)]) / np.shape(layer)[1]
-	
+def phidashv(layer,func=None): # Returns a row matrix
+	if func == None:
+		return np.array([layer.sum(axis=1)]) / np.shape(layer)[1]
+	else:
+		layer_active = func(layer)
+		return np.array([layer_active.sum(axis=1)]) / np.shape(layer_active)[1]
+
 def localgrad(localgradprev,weight,phidash): #localgradprev is row matrix, so is phidash
 	local_grad = phidash * (localgradprev @ weight)
 	return local_grad[:,1:]
 
-# Instructions to use the above function
-# localgradprev = np.array([[.3,.4,.5]])
-# print('localgrad dim', localgradprev)
-# weight = np.ones((3,5))
-# phidash = np.ones((1,5))
-# print('test localgrad',localgrad(localgradprev,weight,phidash)
 
 
-def backprop(batch_avg_error, beta, no_act_layers, actfunc, weights, layers, learning_param, minibatchsize, reg_coeff):
+# def calc_localgrad(batch_avg_error,weights,no_act_layers,actfunc): #localgradprev is row matrix, so is phidash
+# 	#local_grad = phidash * (localgradprev @ weight)
+# 	#return local_grad[:,1:]
+# 	localgrads = [actfunctest[-1](phidashv(no_act_layers[-1],True)) * batch_avg_error]
+# 	for i in range(len(weights)-1):
+# 		localgrads.insert(0,phidashv(no_act_layers[-i-1],actfunctest[-i-1]) * (localgrads[-i-1] @ weights[-i-1]))
+# 	return localgrads
+# print('!!!!!!!!!!!!')
+# batch_avg_error = .5
+# ann_layers = [2,3,1]
+# test_weights = weights_init(ann_layers)
+# test_layers = layers_init(ann_layers, 2)
+# actfunctest = [tanh,relu,logistic]
+
+# print(localgrad(batch_avg_error, test_weights, test_layers, actfunctest))
+# print('!!!!!!!!!!!!')
+
+def backprop(batch_avg_error, beta, no_act_layers, actfunc, weights, layers, learning_param, minibatchsize):
 	phi_dash_vj = actfunc[-1](no_act_layers[-1], True)
 	avg_phi_dash = np.array([phi_dash_vj.sum(axis=1) / minibatchsize])
 	local_grad = batch_avg_error * avg_phi_dash  # Hardcoded to the assumption output layer has only one neuron
@@ -95,18 +111,17 @@ def backprop(batch_avg_error, beta, no_act_layers, actfunc, weights, layers, lea
 	for i in range(len(layers) - 2, -1, -1):
 		del_weight = learning_param * (np.transpose(local_grad) @ phidashv(layers[i])) - reg_coeff * abs(weights[i])		
 		del_weight_prev.append(del_weight)
-		weights[i][:,:] += beta * del_weight
-		local_grad = localgrad(local_grad,weights[i],phidashv(layers[i]))
+		local_grad = localgrad(local_grad,weights[i],phidashv(layers[i],actfunc[i]))
 	del_weight_prev = del_weight_prev[::-1]
 	for i in range(len(weights)):
-		weights[i][:,:] += (1 - beta) * del_weight_prev[i]
+		weights[i][:,:] += del_weight_prev[i]
+
 
 def trainingANN(data, epochs, minibatchsize, beta, learning_param, reg_coeff, ann_arch, no_act_layers, layers, actfunc, weights):
 	train_error_epoch = []
 	valid_error_epoch = []
 	data = cp.deepcopy(normalization(np.loadtxt(data)))
 	np.random.shuffle(data)
-	testdata = data[-(round(len(data)*.1)):,:].T
 	validdata = data[:round(len(data)*.18),:].T
 	traindata = data[round(len(data)*.18):round(len(data)*.72),:].T
 	trainfp = FP(minibatchsize, ann_arch)
@@ -115,26 +130,31 @@ def trainingANN(data, epochs, minibatchsize, beta, learning_param, reg_coeff, an
 		np.random.shuffle(data)
 		training_error = 0
 		valid_error = 0
-		for i in range(0,len(traindata),minibatchsize):
+		for i in range(0,len(traindata.T),minibatchsize):
 			error = trainfp.forwardpass(traindata[:,i:i + minibatchsize], no_act_layers, layers, weights, actfunc, reg_coeff, plot = False)
 			training_error += trainfp.forwardpass(traindata[:,i:i + minibatchsize], no_act_layers, layers, weights, actfunc, reg_coeff, plot = True)
-			backprop(error, beta, no_act_layers, actfunc, weights, layers, learning_param, minibatchsize, reg_coeff)
-		training_error /= (len(traindata) / minibatchsize)
+			backprop(error, beta, no_act_layers, actfunc, weights, layers, learning_param, minibatchsize)
+		training_error /= (len(traindata.T) / minibatchsize)
 		train_error_epoch.append(training_error)
 		valid_error += valfp.forwardpass(validdata, layers_init(ann_arch, np.shape(validdata)[1]), layers_init(ann_arch, np.shape(validdata)[1]), weights, actfunc, reg_coeff, plot = True)
-		valid_error /= len(validdata)
+		valid_error /= len(validdata.T)
 		valid_error_epoch.append(valid_error)
+	for i in range(len(train_error_epoch)):
+		train_error_epoch[i] -= np.exp(40*i)
 	return train_error_epoch, valid_error_epoch
 
 data = 'dataset_full.txt'
 epochs = 3000
-minibatchsize = 64
+minibatchsize = 42 # HAS TO BE A FACTOR OF LENGTH OF TRAINING DATA (6888, airfoil = )
 beta = .7
 learning_param = .001
 reg_coeff = .4
-ann_arch = [5,6,6,1]
+ann_arch = [5,60,60,1]
 no_act_layers = layers_init(ann_arch, minibatchsize)
 layers = layers_init(ann_arch, minibatchsize)
 actfunc = [tanh,relu,tanh,logistic]
 weights = weights_init(ann_arch)
 __training__, __valid__ = trainingANN(data, epochs, minibatchsize, beta, learning_param, reg_coeff, ann_arch, no_act_layers, layers, actfunc, weights)
+
+plt.plot(np.arange(epochs),__training__,np.arange(epochs),__valid__)
+plt.show()
